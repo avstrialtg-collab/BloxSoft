fec714d4fad901e4ba1b754185a6c8d386b773db
avstrialtg-collab <avstrial.tg@gmail.com>
2026-04-20 04:47:08 +0300

    Update speed.lua

--- Module/Misc/speed.lua
+++ Module/Misc/speed.lua
@@ -1,34 +1,36 @@
 local SpeedModule = {
     Name = "WalkSpeed",
-    CurrentValue = 100
+    CurrentValue = 100,
+    Enabled = false
 }
 
--- Главная функция включения/выключения
+local RunService = game:GetService("RunService")
+local player = game.Players.LocalPlayer
+
+-- Постоянный цикл для обхода проверок игры
+RunService.Stepped:Connect(function()
+    if SpeedModule.Enabled then
+        local char = player.Character
+        local hum = char and char:FindFirstChildOfClass("Humanoid")
+        if hum then
+            hum.WalkSpeed = SpeedModule.CurrentValue
+        end
+    end
+end)
+
 SpeedModule.Callback = function(state)
-    local player = game.Players.LocalPlayer
-    local hum = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
-    if hum then
-        hum.WalkSpeed = state and SpeedModule.CurrentValue or 16
+    SpeedModule.Enabled = state
+    -- Возвращаем стандартную скорость при выключении
+    if not state then
+        local char = player.Character
+        local hum = char and char:FindFirstChildOfClass("Humanoid")
+        if hum then hum.WalkSpeed = 16 end
     end
 end
 
--- Функция настройки слайдеров и боксов
 SpeedModule.SetupSettings = function(settings)
     settings.AddSlider(16, 500, 100, function(val)
         SpeedModule.CurrentValue = val
-        -- Если чит включен, сразу обновляем скорость
-        local hum = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-        if hum and hum.WalkSpeed ~= 16 then
-            hum.WalkSpeed = val
-        end
-    end)
-    
-    settings.AddTextBox("Custom Value...", function(text)
-        local num = tonumber(text)
-        if num then
-            SpeedModule.CurrentValue = num
-            print("Speed set to:", num)
-        end
     end)
 end
 
